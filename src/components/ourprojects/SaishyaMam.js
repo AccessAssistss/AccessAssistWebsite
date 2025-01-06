b7bd678a5b868632301182706efb7dd95c638a3a
AccessAssistss <postbox07@gmail.com>
2025-01-06 16:55:30 +0530

    Update SaishyaMam.js

--- src/components/ourprojects/SaishyaMam.js
+++ src/components/ourprojects/SaishyaMam.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const SaishyaMam = () => {
   return (
-    <div><iframe src="https://app.powerbi.com/view?r=eyJrIjoiYWFjYzQyM2QtOWIzNS00ODMzLThjMTgtNGUzMTNjZTdmN2EwIiwidCI6IjM0YmQ4YmVkLTJhYzEtNDFhZS05ZjA4LTRlMGEzZjExNzA2YyJ9&pageName=d94d0d00296848009950" title="website"></iframe></div>
-  )
-}
+    <div style={{ margin: 0, padding: 0, height: '100vh', width: '100vw', overflow: 'hidden' }}>
+      <iframe
+        src="https://app.powerbi.com/view?r=eyJrIjoiYWFjYzQyM2QtOWIzNS00ODMzLThjMTgtNGUzMTNjZTdmN2EwIiwidCI6IjM0YmQ4YmVkLTJhYzEtNDFhZS05ZjA4LTRlMGEzZjExNzA2YyJ9&pageName=d94d0d00296848009950"
+        title="website"
+        style={{
+          border: 'none',
+          height: '100%',
+          width: '100%',
+        }}
+      ></iframe>
+    </div>
+  );
+};
 
-export default SaishyaMam
+export default SaishyaMam;
